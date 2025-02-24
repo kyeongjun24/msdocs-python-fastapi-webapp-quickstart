e3c6d5f1e78a3ed86dfe406c73616bba04c88588
lee <kj27@msctaz.com>
2025-02-24 09:53:17 +0900

    0224 commit

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from fastapi.responses import HTMLResponse, FileResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import uvicorn
-
+import urllib.request
+import json
+import os
+import ssl
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -32,3 +35,66 @@ async def hello(request: Request, name: str = Form(...)):
 if __name__ == '__main__':
     uvicorn.run('main:app', host='0.0.0.0', port=8000)
 
+
+def allowSelfSignedHttps(allowed):
+    # bypass the server certificate verification on client side
+    if allowed and not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None):
+        ssl._create_default_https_context = ssl._create_unverified_context
+
+allowSelfSignedHttps(True) # this line is needed if you use self-signed certificate in your scoring service.
+
+
+# ID/PW 인증 : sharepoint내 파일 리스트 조회
+@app.get("/list")
+async def list(request: Request, question:str = 'donald trump'):
+
+    url = 'https://aipjt-sharepoint-search-odqhq.koreacentral.inference.ml.azure.com/score'
+    api_key = 'd2vfEOcV3iXUiGqXiR7h0BAcwoQovX7BhBApPxAOkpT9Estu8Xy9JQQJ99BBAAAAAAAAAAAAINFRAZML4FyQ'
+    if not api_key:
+        raise Exception("A key should be provided to invoke the endpoint")
+
+    data = {"question": question}
+    body = str.encode(json.dumps(data))
+    headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
+    req = urllib.request.Request(url, body, headers)
+
+    try:
+        response = urllib.request.urlopen(req)
+        result = response.read()
+        print(result)
+        return result
+    except urllib.error.HTTPError as error:
+        print("The request failed with status code: " + str(error.code))
+        print(error.info())
+        print(error.read().decode("utf8", 'ignore'))
+        return error.info()
+
+
+# Index 사용 : sharepoint 질의응답
+@app.get("/index")
+async def index_search(request: Request, question:str = ''):
+
+    url = 'https://test-0116.koreacentral.inference.ml.azure.com/score'
+    api_key = 'j2pgwSOFeaIzw3cA8ypSP2KvVSBh6R0w'
+    if not api_key:
+        raise Exception("A key should be provided to invoke the endpoint")
+
+    data = {"question1": question}
+    body = str.encode(json.dumps(data))
+    headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
+    req = urllib.request.Request(url, body, headers)
+
+    try:
+        response = urllib.request.urlopen(req)
+        result = response.read()
+        print(result)
+        return result
+    except urllib.error.HTTPError as error:
+        print("The request failed with status code: " + str(error.code))
+        print(error.info())
+        print(error.read().decode("utf8", 'ignore'))
+        return error.info()
+
+
+# token으로 sharepoint 조회하는 기능 추가 예정
+# 02/19 : 401 에러 발생
